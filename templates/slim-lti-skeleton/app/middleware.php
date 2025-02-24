330f1b74b23aab2ba18e970e487bb8efca70b5f8
Seth Battis <sbattis@groton.org>
2025-02-24 12:56:19 -0500

    feat(slim-lti-skeleton): merge in casbin/casbin-with-slim Authorization middleware

--- templates/slim-lti-skeleton/app/middleware.php
+++ templates/slim-lti-skeleton/app/middleware.php
@@ -2,6 +2,21 @@
 
 declare(strict_types=1);
 
+use App\Middleware\Authorization;
+use Tuupola\Middleware\HttpBasicAuthentication;
 use Slim\App;
 
-return function (App $app) {};
+return function (App $app) {
+    $app->add(Authorization::class);
+
+    $app->add(new HttpBasicAuthentication([
+        'users' => [
+            // TODO **DEPLOY** Real users
+            'root' => 't00r',
+            'somebody' => 'passw0rd',
+        ],
+        'before' => function ($request, $arguments) {
+            return $request->withAttribute('user', $arguments['user']);
+        },
+    ]));
+};
