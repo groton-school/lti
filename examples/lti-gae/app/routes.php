9f8da98f008f39a2bef47df75370ac037499a2b2
Seth Battis <sbattis@groton.org>
2025-07-03 09:07:49 -0400

    feat: add example
    Also good for testing

---
+++ examples/lti-gae/app/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use GrotonSchool\Slim\GAE\Actions\EmptyAction;
+use GrotonSchool\Slim\LTI\Actions\JWKSAction;
+use GrotonSchool\Slim\LTI\Actions\LaunchAction;
+use GrotonSchool\Slim\LTI\Actions\LoginAction;
+use GrotonSchool\Slim\LTI\Actions\RegistrationStartAction;
+use GrotonSchool\Slim\LTI\Actions\ThirdPartyCookieAction;
+use Odan\Session\Middleware\SessionStartMiddleware;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    // return an empty string on GAE start/stop requests
+    $app->get('/_ah/{action:.*}', EmptyAction::class);
+
+    // standard LTI endpoints
+    $app->group('/lti', function (Group $lti) {
+        $lti->post('/launch', LaunchAction::class);
+        $lti->get('/jwks', JWKSAction::class);
+        $lti->get('/register', RegistrationStartAction::class);
+        $lti->post('/login', LoginAction::class);
+    })->add(SessionStartMiddleware::class);
+};
